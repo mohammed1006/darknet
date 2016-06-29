25525d67826e5398f42af456a3bb9a110b0bd6ac
Quan Hua <quanhua92@gmail.com>
2016-06-29 15:40:10 +0700

    Remove empty labels

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -6,7 +6,8 @@ from os.path import join
 
 sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+#classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["person"]
 
 
 def convert(size, box):
@@ -31,16 +32,21 @@ def convert_annotation(year, image_id):
     w = int(size.find('width').text)
     h = int(size.find('height').text)
 
+    has_result = False
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
         if cls not in classes or int(difficult) == 1:
             continue
+        has_result = True
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
         bb = convert((w,h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+    if has_result == False:
+        os.remove('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id))
+    return has_result
 
 wd = getcwd()
 
@@ -50,7 +56,7 @@ for year, image_set in sets:
     image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
-        convert_annotation(year, image_id)
+        has_result = convert_annotation(year, image_id)
+        if has_result:
+            list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
     list_file.close()
-

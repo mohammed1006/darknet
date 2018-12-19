59d860816de1c4b100b4645338a7a8a6d07cde5e
Koos du Preez <35713435+kooscode@users.noreply.github.com>
2018-12-18 21:25:27 -0800

    Update pullmergemaster.sh

--- pullmergemaster.sh
+++ pullmergemaster.sh
@@ -1,4 +1,6 @@
 
+# https://help.github.com/articles/merging-an-upstream-repository-into-your-fork/
+clear
 echo Merging from AlexeyAB \"master\"
 git checkout master
 git pull https://github.com/AlexeyAB/darknet.git master

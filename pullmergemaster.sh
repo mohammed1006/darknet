2855f044729b563c938ec12825b6727057e62f23
Koos du Preez <koos.dupreez@terraclear.com>
2019-06-12 14:50:46 -0700

    merged with AlexeyAB latest

--- pullmergemaster.sh
+++ pullmergemaster.sh
@@ -7,4 +7,5 @@ git pull https://github.com/AlexeyAB/darknet.git master
 
 echo Need to manually add and push..
 # git add .
+# git commit -m "Merged with latest AlexeyAB master"
 # git push origin master

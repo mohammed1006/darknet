199121fa09a1905a19b34e6779d3d9574d1598b6
sdirollo <sebastien.dirollo@gmail.com>
2020-03-02 17:52:16 +0100

    Add mount image to sambashare

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo nvidia/docker run -it \
+sudo nvidia-docker run -it \
 	-v /media/sambashare/Exchange/training_samples/synthetic:/darknet/data/synthetic \
 	# docker image name
   12f9520d7112 \

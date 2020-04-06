b2ff6eb34d0fd3ae0e6943ae3ba097e6529d46af
Diana <rusak.di@students.dvfu.ru>
2020-04-07 01:00:54 +1000

    Add ability to pause video by pressing 'space'
    Pause works for video and camera images by 'space' button

--- src/demo.c
+++ src/demo.c
@@ -206,10 +206,15 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
     double start_time = get_time_point();
     float avg_fps = 0;
     int frame_counter = 0;
-
+    int flag_pause = 0;
     while(1){
         ++count;
         {
+            if (flag_pause != 0) {
+                int c = wait_key_cv(1);
+                flag_pause = c != 32;
+                continue;
+            }
             const float nms = .45;    // 0.4F
             int local_nboxes = nboxes;
             detection *local_dets = dets;
@@ -264,6 +269,7 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
                     {
                         flag_exit = 1;
                     }
+                    flag_pause = c == 32;
                 }
             }else{
                 char buff[256];

8a54c7921f48ed44710da9f8eb3657bdd46cc5dd
Victor Terra Ferrao <victor.ferrao@datatraffic.com.br>
2020-06-26 00:40:47 -0300

    Removed duplicated code; Updated the constructor to be more friendly

--- src/parser.h
+++ src/parser.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 network parse_network_cfg(char *filename);
-network parse_network_cfg_mem(char *str);
+network parse_network_cfg_mem(const char *str);
 network parse_network_cfg_custom(char *filename, int batch, int time_steps);
-network parse_network_cfg_custom_mem(char *str, int batch, int time_steps);
+network parse_network_cfg_custom_mem(const char *str, int batch, int time_steps);
 void save_network(network net, char *filename);
 void save_weights(network net, char *filename);
 void save_weights_upto(network net, char *filename, int cutoff);
 void save_weights_double(network net, char *filename);
 void load_weights(network *net, char *filename);
-void load_weights_mem(network *net, char *data);
+void load_weights_mem(network *net, const char *data, int data_size);
 void load_weights_upto(network *net, char *filename, int cutoff);
-void load_weights_upto_mem(network *net, char *data, int cutoff);
+void load_weights_upto_mem(network *net, const char *data, int data_size, int cutoff);
 
 #ifdef __cplusplus
 }

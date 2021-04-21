077509109d4f1f3512e583c5a12a526c941bde41
Stefano Sinigardi <stesinigardi@hotmail.com>
2021-04-21 16:00:42 +0200

    add error messages during build to not have green badge when failed

--- scripts/deploy-cuda.ps1
+++ scripts/deploy-cuda.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-$CudaUrl = 'https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe'
+$url = 'https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe'
 
 $CudaFeatures = 'nvcc_11.3 cuobjdump_11.3 nvprune_11.3 cupti_11.3 memcheck_11.3 nvdisasm_11.3 nvprof_11.3 ' + `
  'visual_studio_integration_11.3 visual_profiler_11.3 visual_profiler_11.3 cublas_11.3 cublas_dev_11.3 ' + `
@@ -10,20 +10,17 @@ $CudaFeatures = 'nvcc_11.3 cuobjdump_11.3 nvprune_11.3 cupti_11.3 memcheck_11.3 
 
 try {
   Write-Host 'Downloading CUDA...'
-  [string]$installerPath = Get-TempFilePath -Extension 'exe'
-  curl.exe -L -o $installerPath -s -S $CudaUrl
+  Invoke-WebRequest -Uri $url -OutFile "cuda_11.3.0_win10_network.exe"
   Write-Host 'Installing CUDA...'
-  $proc = Start-Process -FilePath $installerPath -ArgumentList @('-s ' + $CudaFeatures) -Wait -PassThru
+  $proc = Start-Process -Wait -PassThru -FilePath "./cuda_11.3.0_win10_network.exe" -ArgumentList @('-s ' + $CudaFeatures)
   $exitCode = $proc.ExitCode
   if ($exitCode -eq 0) {
     Write-Host 'Installation successful!'
   }
   else {
-    Write-Error "Installation failed! Exited with $exitCode."
-    throw
+    Throw "Installation failed! Exited with $exitCode."
   }
 }
 catch {
-  Write-Error "Failed to install CUDA! $($_.Exception.Message)"
-  throw
+  Throw "Failed to install CUDA! $($_.Exception.Message)"
 }

d1465ee98c7981f9aee96bde2d12cd78c1dc02ca
Stefano Sinigardi <stesinigardi@hotmail.com>
2021-06-03 14:34:16 +0200

    setup CUDA env variables

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -16,6 +16,9 @@ Push-Location $PSScriptRoot
 
 if ($InstallCUDA) {
   & $PSScriptRoot/deploy-cuda.ps1
+  $env:CUDA_PATH="C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.3"
+  $env:CUDA_TOOLKIT_ROOT_DIR="C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.3"
+  $env:CUDACXX="C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.3\\bin\\nvcc.exe"
   $CUDAisAvailable = $true
 }
 else {

c034bfa33823c0e45eb2f854e8bf85ba649aecc0
Stefano Sinigardi <stesinigardi@hotmail.com>
2021-06-03 11:19:12 +0200

    improve setup.sh script for more tool installations and add command line flags handling

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,36 +1,54 @@
 #!/usr/bin/env bash
 
-## enable or disable installed components
+install_tools=false
 
-install_cuda=true
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-###########################
+case $key in
+    -InstallCUDA|--InstallCUDA)
+    install_tools=true
+    shift
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $script_dir/..
 temp_folder="./temp"
-cuda_is_available=false
 mkdir -p $temp_folder
 cd $temp_folder
 
-sudo apt-get install cmake git ninja-build build-essential g++
-
-if [ "$install_cuda" = true ] ; then
+if [ "$install_tools" = true ] ; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Unable to provide CUDA on macOS"
-    cuda_is_available=false
+    echo "Unable to provide tools on macOS, please wait for a future script update or do not put -InstallCUDA command line flag to continue"
   else
-    # Download and install CUDA
     if [[ $(cut -f2 <<< $(lsb_release -r)) == "18.04" ]]; then
+      sudo apt-get update
+      sudo apt-get install git ninja-build build-essential g++
+      sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget
       wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
       sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
       sudo dpkg -i cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
       wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
       sudo dpkg -i nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-      sudo apt update
+      sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+      wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+      sudo dpkg -i packages-microsoft-prod.deb
+      sudo add-apt-repository universe
+      sudo apt-get update
       sudo apt-get dist-upgrade -y
       sudo apt-get install -y --no-install-recommends cuda-compiler-10-2 cuda-libraries-dev-10-2 cuda-driver-dev-10-2 cuda-cudart-dev-10-2 cuda-curand-dev-10-2
       sudo apt-get install -y --no-install-recommends libcudnn7-dev
+      sudo apt-get install -y cmake
+      sudo apt-get install -y powershell
       sudo rm -rf /usr/local/cuda
       sudo ln -s /usr/local/cuda-10.2 /usr/local/cuda
       export PATH=/usr/local/cuda/bin:$PATH
@@ -40,15 +58,23 @@ if [ "$install_cuda" = true ] ; then
       export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
       cuda_is_available=true
     elif [[ $(cut -f2 <<< $(lsb_release -r)) == "20.04" ]]; then
-      sudo apt update
-      sudo apt-get dist-upgrade -y
-      #sudo apt-get install -y --no-install-recommends nvidia-cuda-dev nvidia-cuda-toolkit
+      sudo apt-get update
+      sudo apt-get install git ninja-build build-essential g++
+      sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget
       sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
       sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+      sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
       sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
       sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/ /"
+      wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+      sudo dpkg -i packages-microsoft-prod.deb
+      sudo add-apt-repository universe
+      sudo apt-get update
+      sudo apt-get dist-upgrade -y
       sudo apt-get install -y --no-install-recommends cuda-compiler-11-2 cuda-libraries-dev-11-2 cuda-driver-dev-11-2 cuda-cudart-dev-11-2
       sudo apt-get install -y --no-install-recommends libcudnn8-dev
+      sudo apt-get install -y cmake
+      sudo apt-get install -y powershell
       sudo rm -rf /usr/local/cuda
       sudo ln -s /usr/local/cuda-11.2 /usr/local/cuda
       export PATH=/usr/local/cuda/bin:$PATH
@@ -58,22 +84,15 @@ if [ "$install_cuda" = true ] ; then
       export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
       cuda_is_available=true
     else
-      echo "Unable to auto-install CUDA on this Linux OS"
-      cuda_is_available=false
+      echo "Unable to provide tools on macOS, please wait for a future script update or do not put -InstallCUDA command line flag to continue"
     fi
-  fi
-else
-  if [[ -v CUDA_PATH ]]; then
-    cuda_is_available=true
-  else
-    cuda_is_available=false
   fi
 fi
 
 cd ..
 rm -rf "$temp_folder"
 
-if [ "$cuda_is_available" = true ] ; then
+if [[ -v CUDA_PATH ]]; then
   ./build.ps1 -UseVCPKG -EnableOPENCV -EnableCUDA -EnableOPENCV_CUDA
 else
   ./build.ps1 -UseVCPKG -EnableOPENCV

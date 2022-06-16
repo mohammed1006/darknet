62d2380acb876a9ebe7003b6270f2d75c81a614d
CristiFati <fati_utcluj@yahoo.com>
2022-06-17 00:30:13 +0300

    Fix multiple code readability issues

--- darknet_video.py
+++ darknet_video.py
@@ -1,11 +1,10 @@
-from ctypes import *
 import random
 import os
 import cv2
 import time
 import darknet
 import argparse
-from threading import Thread, enumerate
+import threading
 import queue
 
 
@@ -64,10 +63,10 @@ def convert2relative(bbox):
     """
     YOLO format use relative coordinates for annotation
     """
-    x, y, w, h  = bbox
-    _height     = darknet_height
-    _width      = darknet_width
-    return x/_width, y/_height, w/_width, h/_height
+    x, y, w, h = bbox
+    _height = darknet_height
+    _width = darknet_width
+    return x / _width, y / _height, w / _width, h / _height
 
 
 def convert2original(image, bbox):
@@ -75,10 +74,10 @@ def convert2original(image, bbox):
 
     image_h, image_w, __ = image.shape
 
-    orig_x       = int(x * image_w)
-    orig_y       = int(y * image_h)
-    orig_width   = int(w * image_w)
-    orig_height  = int(h * image_h)
+    orig_x = int(x * image_w)
+    orig_y = int(y * image_h)
+    orig_width = int(w * image_w)
+    orig_height = int(h * image_h)
 
     bbox_converted = (orig_x, orig_y, orig_width, orig_height)
 
@@ -90,22 +89,26 @@ def convert4cropping(image, bbox):
 
     image_h, image_w, __ = image.shape
 
-    orig_left    = int((x - w / 2.) * image_w)
-    orig_right   = int((x + w / 2.) * image_w)
-    orig_top     = int((y - h / 2.) * image_h)
-    orig_bottom  = int((y + h / 2.) * image_h)
+    orig_left = int((x - w / 2.) * image_w)
+    orig_right = int((x + w / 2.) * image_w)
+    orig_top = int((y - h / 2.) * image_h)
+    orig_bottom = int((y + h / 2.) * image_h)
 
-    if (orig_left < 0): orig_left = 0
-    if (orig_right > image_w - 1): orig_right = image_w - 1
-    if (orig_top < 0): orig_top = 0
-    if (orig_bottom > image_h - 1): orig_bottom = image_h - 1
+    if orig_left < 0:
+        orig_left = 0
+    if orig_right > image_w - 1:
+        orig_right = image_w - 1
+    if orig_top < 0:
+        orig_top = 0
+    if orig_bottom > image_h - 1:
+        orig_bottom = image_h - 1
 
     bbox_cropping = (orig_left, orig_top, orig_right, orig_bottom)
 
     return bbox_cropping
 
 
-def video_capture(frame_queue, darknet_image_queue):
+def video_capture(raw_frame_queue, preprocessed_frame_queue):
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -113,16 +116,16 @@ def video_capture(frame_queue, darknet_image_queue):
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_resized = cv2.resize(frame_rgb, (darknet_width, darknet_height),
                                    interpolation=cv2.INTER_LINEAR)
-        frame_queue.put(frame)
+        raw_frame_queue.put(frame)
         img_for_detect = darknet.make_image(darknet_width, darknet_height, 3)
         darknet.copy_image_from_bytes(img_for_detect, frame_resized.tobytes())
-        darknet_image_queue.put(img_for_detect)
+        preprocessed_frame_queue.put(img_for_detect)
     cap.release()
 
 
-def inference(darknet_image_queue, detections_queue, fps_queue):
+def inference(preprocessed_frame_queue, detections_queue, fps_queue):
     while cap.isOpened():
-        darknet_image = darknet_image_queue.get()
+        darknet_image = preprocessed_frame_queue.get()
         prev_time = time.time()
         detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh)
         detections_queue.put(detections)
@@ -134,11 +137,11 @@ def inference(darknet_image_queue, detections_queue, fps_queue):
     cap.release()
 
 
-def drawing(frame_queue, detections_queue, fps_queue):
+def drawing(raw_frame_queue, detections_queue, fps_queue):
     random.seed(3)  # deterministic bbox colors
     video = set_saved_video(cap, args.out_filename, (video_width, video_height))
     while cap.isOpened():
-        frame = frame_queue.get()
+        frame = raw_frame_queue.get()
         detections = detections_queue.get()
         fps = fps_queue.get()
         detections_adjusted = []
@@ -148,7 +151,7 @@ def drawing(frame_queue, detections_queue, fps_queue):
                 detections_adjusted.append((str(label), confidence, bbox_adjusted))
             image = darknet.draw_boxes(detections_adjusted, frame, class_colors)
             if not args.dont_show:
-                cv2.imshow('Inference', image)
+                cv2.imshow("Inference", image)
             if args.out_filename is not None:
                 video.write(image)
             if cv2.waitKey(fps) == 27:
@@ -158,26 +161,25 @@ def drawing(frame_queue, detections_queue, fps_queue):
     cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
-    frame_queue = queue.Queue()
-    darknet_image_queue = queue.Queue(maxsize=1)
+if __name__ == "__main__":
+    raw_frame_queue = queue.Queue()
+    preprocessed_frame_queue = queue.Queue(maxsize=1)
     detections_queue = queue.Queue(maxsize=1)
     fps_queue = queue.Queue(maxsize=1)
 
     args = parser()
     check_arguments_errors(args)
     network, class_names, class_colors = darknet.load_network(
-            args.config_file,
-            args.data_file,
-            args.weights,
-            batch_size=1
-        )
+        args.config_file,
+        args.data_file,
+        args.weights,
+        batch_size=1)
     darknet_width = darknet.network_width(network)
     darknet_height = darknet.network_height(network)
     input_path = str2int(args.input)
     cap = cv2.VideoCapture(input_path)
     video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
-    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
-    Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
+    threading.Thread(target=video_capture, args=(raw_frame_queue, preprocessed_frame_queue)).start()
+    threading.Thread(target=inference, args=(preprocessed_frame_queue, detections_queue, fps_queue)).start()
+    threading.Thread(target=drawing, args=(raw_frame_queue, detections_queue, fps_queue)).start()
